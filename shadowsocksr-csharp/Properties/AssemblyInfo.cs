7d15de3bc62c2dc79d1bd6378aaeec689e4616a3
KoakiMiku <2234975857@qq.com>
2018-07-12 16:39:59 +0800

    fix: name

--- shadowsocksr-csharp/Properties/AssemblyInfo.cs
+++ shadowsocksr-csharp/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ShadowsocksS")]
-[assembly: AssemblyDescription("ShadowsocksS")]
-[assembly: AssemblyProduct("ShadowsocksS")]
+[assembly: AssemblyTitle("ShadowsocksR")]
+[assembly: AssemblyDescription("ShadowsocksR")]
+[assembly: AssemblyProduct("ShadowsocksR")]
 [assembly: AssemblyCopyright("Copyright © KoakiMiku 2018, Fork from ShadowsocksR by BreakWa11")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f8334709-4309-436a-8bbd-6165dcf4a660")]
